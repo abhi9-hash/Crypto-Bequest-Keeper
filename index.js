1250a4809b9a320be0629bf07f9692075ab053bb
abhnv_pthk <coolabhinav57@gmail.com>
2021-11-16 20:35:08 +0530

    server update

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.listen(process.env.PORT||5000,async()=>{
                   requireTLS: true,
                   auth: {
                       user: 'abhinavvpathakk@gmail.com',
-                      pass: '8077399393'
+                      pass: process.env.PASSWORD
                   }
               });
                 var mailOptions = {
@@ -71,7 +71,7 @@ app.listen(process.env.PORT||5000,async()=>{
                 requireTLS: true,
                 auth: {
                     user: 'abhinavvpathakk@gmail.com',
-                    pass: '8077399393'
+                    pass: process.env.PASSWORD
                 }
             });
               var mailOptions = {
